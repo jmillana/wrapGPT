1ab690fff351cea993bcae90eedd2998c88952ef
jmillana <jordi@marimon.tech>
2023-03-30 23:26:13 +0200

    Create main.py
    Defines the entrypoint to the chatbot

---
+++ main.py
@@ -0,0 +1,15 @@
+"""Entry point for the chatbot application."""
+import os
+
+import dotenv
+import openai
+import typer
+
+from src import cli
+
+dotenv.load_dotenv()
+
+openai.api_key = os.getenv("OPENIA_API_KEY")
+
+if __name__ == "__main__":
+    typer.run(cli.run)
